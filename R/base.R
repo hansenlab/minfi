e90eec72b46cf760b163bee89a113f0dd8496951
khansen <khansen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-02-23 14:32:02 +0000

    added constructors; added version fields to the base classes (use updateObject to update old objects)
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/minfi@63007 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/base.R
@@ -0,0 +1,90 @@
+getManifest <- function(object) {
+    name <- paste(annotation(object), "manifest", sep = "")
+    get(name)
+}
+
+getProbeData <- function(object) {
+    if(is(object, "IlluminaMethylationManifest"))
+        return(object@data)
+    if(is(object, "RGChannelSet"))
+        return(getManifest(object)@data)
+    stop("cannot handle 'object'")
+}    
+
+getProbeInfo <- function(object, type = c("I", "II", "Control", "I-Green", "I-Red")) {
+    type <- match.arg(type)
+    if(type %in% c("I", "II", "Control"))
+        return(getProbeData(object)[[paste("Type", type, sep = "")]])
+    typeI <- getProbeData(object)[["TypeI"]]
+    if(type == "I-Green")
+        return(typeI[typeI$Color == "Grn",])
+    if(type == "I-Red")
+        return(typeI[typeI$Color == "Red",])
+}
+
+getManifestInfo <- function(object, type = c("nLoci", "locusNames")) {
+    type <- match.arg(type)
+    switch(type,
+           "nLoci" = {
+               nrow(getProbeInfo(object, type = "I")) +
+                   nrow(getProbeInfo(object, type = "II"))
+           },
+           "locusNames" = {
+               c(getProbeInfo(object, type = "I")$Name,
+                 getProbeInfo(object, type = "II")$Name)
+           })
+}
+
+getControlAddress <- function(object, controlType = c("NORM_A", "NORM_C", "NORM_G", "NORM_T")) {
+    ctrls <- getProbeInfo(object, type = "Control")
+    ctrls[ctrls$Type %in% controlType, "Address"]
+}
+
+getRed <- function(object) {
+    if(!is(object, "RGChannelSet"))
+        stop("'object' needs to be a 'RGChannelSet'")
+    assayDataElement(object, "Red")
+}
+
+getGreen <- function(object) {
+    if(!is(object, "RGChannelSet"))
+        stop("'object' needs to be a 'RGChannelSet'")
+    assayDataElement(object, "Green")
+}
+
+getMeth <- function(object) {
+    if(!is(object, "MethylSet"))
+        stop("'object' needs to be an 'MethylSet'")
+    assayDataElement(object, "Meth")
+}
+
+getUnmeth <- function(object) {
+    if(!is(object, "MethylSet"))
+        stop("'object' needs to be an 'MethylSet'")
+    assayDataElement(object, "Unmeth")
+}
+
+getBeta <- function(object, type = c("minfi", "Illumina")) {
+    if(!is(object, "MethylSet") & !is(object, "RGChannelSet"))
+        stop("'object' needs to be an 'MethylSet' or a 'RGChannelSet'")
+    type <- match.arg(type)
+    if(is(object, "RGChannelSet"))
+        object <- preprocessRaw(object)
+    Meth <- pmax(assayDataElement(object, "Meth"), 0)
+    Unmeth <- pmax(assayDataElement(object, "Unmeth"), 0)
+    if(type == "minfi")
+        return(Meth / (Meth + Unmeth))
+    if(type == "Illumina")
+        return(Meth / (Meth + Unmeth + 100))
+}
+
+getM <- function (object, type = c("minfi", "Illumina")) {
+    if(!is(object, "MethylSet") & !is(object, "RGChannelSet"))
+        stop("'object' needs to be an 'MethylSet' or a 'RGChannelSet'")
+    type <- match.arg(type)
+    beta <- getBeta(object, type = type)
+    beta <- pmin(beta, 0.999)
+    beta <- pmax(beta, 0.001)
+    logit2(beta)
+}
+
