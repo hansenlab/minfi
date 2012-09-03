1ea0f10695f561585d7373f9f666e74997d60955
Kasper D. Hansen <khansen@stat.Berkeley.edu>
2012-09-03 21:37:12 +0000

    updated GenomeStudio script
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/minfi@69036 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/GenomeStudio.R
+++ inst/scripts/GenomeStudio.R
@@ -1,10 +1,11 @@
 ## This script compares the output of Genome Studio to preprocess.Illumina
 
 require(minfi)
+require(matrixStats)
 
-basepath <- "/thumper2/feinbergLab/core/iScan/Experiments/IL002"
-targets <- read.csv(file.path(basepath, "IL002.csv"), stringsAsFactors = FALSE, skip = 7)
-RGset <- read.450k.exp(basedir = basepath, targets, verbose = TRUE)
+basepath <- "/thumper2/feinbergLab/core/arrays/illumina/IL002"
+sheet <- read.450k.sheet(basepath, pattern = "IL002_v2.csv$")
+RGset <- read.450k.exp(targets = sheet, verbose = TRUE)
 ISet.raw <- preprocessIllumina(RGset, bg.correct = FALSE, normalize = "no")
 ISet.bg <- preprocessIllumina(RGset, bg.correct = TRUE, normalize = "no")
 ISet.norm <- preprocessIllumina(RGset, bg.correct = FALSE, normalize = "controls",
@@ -15,20 +16,20 @@ ISet.norm.bg <- preprocessIllumina(RGset, bg.correct = TRUE,
 od <- c(1,7,8,2,14,9, 3,21,10,15,16,22,
         4,11,23,17,12,24,18,19,5,6,13,20)
 basepath <- "/thumper2/feinbergLab/personal/khansen/450k/data"
-GS.raw <- read.GenomeStudio(file.path(basepath,
+GS.raw <- minfi:::read.GenomeStudio(file.path(basepath,
                                       "FinalReport_noBack_noNorm.txt"))
 odN <- colnames(GS.raw$beta)[od]
 GS.raw <- lapply(GS.raw, function(xx) as.matrix(xx[featureNames(ISet.raw), odN]))
 
-GS.bg <- read.GenomeStudio(file.path(basepath,
+GS.bg <- minfi:::read.GenomeStudio(file.path(basepath,
                                      "FinalReport_yesBack_noNorm.txt"))
 GS.bg <- lapply(GS.bg, function(xx) as.matrix(xx[featureNames(ISet.bg), odN]))
 
-GS.norm <- read.GenomeStudio(file.path(basepath,
+GS.norm <- minfi:::read.GenomeStudio(file.path(basepath,
                                        "FinalReport_noBack_yesNorm.txt"))
 GS.norm <- lapply(GS.norm, function(xx) as.matrix(xx[featureNames(ISet.norm), odN]))
 
-GS.norm.bg <- read.GenomeStudio(file.path(basepath,
+GS.norm.bg <- minfi:::read.GenomeStudio(file.path(basepath,
                                           "FinalReport_yesBack_yesNorm.txt"))
 GS.norm.bg <- lapply(GS.norm.bg, function(xx) as.matrix(xx[featureNames(ISet.norm.bg), odN]))
 
@@ -38,27 +39,27 @@ cmax <- colMaxs(abs(getMeth(ISet.raw) - GS.raw$SignalB))
 round(cmax, 1)
 cmax <- colMaxs(abs(getUnmeth(ISet.raw) - GS.raw$SignalA))
 round(cmax, 1)
-cmax <- colMaxs(abs(getBeta(ISet.raw) - GS.raw$beta), na.rm = TRUE)
+cmax <- colMaxs(abs(getBeta(ISet.raw, type = "Illumina") - GS.raw$beta), na.rm = TRUE)
 round(cmax, 3)
 
 cmax <- colMaxs(abs(getMeth(ISet.bg) - GS.bg$SignalB))
 round(cmax, 1)
 cmax <- colMaxs(abs(getUnmeth(ISet.bg) - GS.bg$SignalA))
 round(cmax, 1)
-cmax <- colMaxs(abs(getBeta(ISet.bg) - GS.bg$beta), na.rm = TRUE)
+cmax <- colMaxs(abs(getBeta(ISet.bg, type = "Illumina") - GS.bg$beta), na.rm = TRUE)
 round(cmax, 3)
 
 cmax <- colMaxs(abs(getMeth(ISet.norm) - GS.norm$SignalB))
 round(cmax, 2)
 cmax <- colMaxs(abs(getUnmeth(ISet.norm) - GS.norm$SignalA))
 round(cmax, 2)
-cmax <- colMaxs(abs(getBeta(ISet.norm) - GS.norm$beta), na.rm = TRUE)
+cmax <- colMaxs(abs(getBeta(ISet.norm, type = "Illumina") - GS.norm$beta), na.rm = TRUE)
 round(cmax, 3)
 
 cmax <- colMaxs(abs(getMeth(ISet.norm.bg) - GS.norm.bg$SignalB))
 round(cmax, 2)
 cmax <- colMaxs(abs(getUnmeth(ISet.norm.bg) - GS.norm.bg$SignalA))
 round(cmax, 2)
-cmax <- colMaxs(abs(getBeta(ISet.norm.bg) - GS.norm.bg$beta), na.rm = TRUE)
+cmax <- colMaxs(abs(getBeta(ISet.norm.bg, type = "Illumina") - GS.norm.bg$beta), na.rm = TRUE)
 round(cmax, 3)
 
