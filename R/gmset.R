78c1a05b7e8332d43d8673cddab47898daa64eed
Kasper D. Hansen <khansen@stat.Berkeley.edu>
2012-09-07 21:52:08 +0000

    version bump and documentation changes
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/minfi@69220 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/gmset.R
+++ R/gmset.R
@@ -1,18 +1,26 @@
 setClass("GenomicMethylSet",
-         representation(preprocessMethod = "character"),
+         representation(preprocessMethod = "character", annotation = "character"),
          contains = "SummarizedExperiment")
 
 ## FIXME: add show method
 ## FIXME: add validity, check for GRanges, not GRangesList, meth, unmeth
 
-GenomicMethylSet <- function(gr, Meth, Unmeth, pData) {
+GenomicMethylSet <- function(gr, Meth, Unmeth, pData, annotation, preprocessMethod) {
     assays <- SimpleList(Meth = Meth, Unmeth = Unmeth)
     assays <- GenomicRanges:::.ShallowSimpleListAssays$new(data = assays)
     colData <- as(pData, "DataFrame")
     rowData <- as(gr, "GRanges")
     new("GenomicMethylSet", assays = assays, colData = colData,
-        rowData = rowData)
+        rowData = rowData, annotation = annotation, preprocessMethod = preprocessMethod)
 }
+
+setMethod("show", signature(object = "GenomicMethylSet"),
+          function(object) {
+              callNextMethod()
+              minfi:::.show.annotation(annotation(object))
+              minfi:::.show.preprocessMethod(preprocessMethod(object))
+          })
+
 
 setMethod("getMeth", signature(object = "GenomicMethylSet"),
           function(object) {
@@ -39,6 +47,16 @@ setMethod("getM", signature(object = "GenomicMethylSet"),
                   return(log2(getMeth(object) / getUnmeth(object)))
               if(type == "beta" || type == "Beta")
                   return(logit2(getBeta(object, ...)))
+          })
+
+setMethod("preprocessMethod", signature(object = "GenomicMethylSet"),
+          function(object) {
+              object@preprocessMethod
+          })
+
+setMethod("annotation", signature(object = "GenomicMethylSet"),
+          function(object) {
+              object@annotation
           })
 
 setMethod("pData", signature("GenomicMethylSet"),
