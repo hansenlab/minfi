d1fafe8b2bcf9f78eb8052368cac57a0760f4e9d
Kasper D. Hansen <khansen@stat.Berkeley.edu>
2012-09-19 21:47:00 +0000

    some minor updates and bugfixes
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/minfi@69592 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/gmset.R
+++ R/gmset.R
@@ -3,7 +3,13 @@ setClass("GenomicMethylSet",
          contains = "SummarizedExperiment")
 
 ## FIXME: add show method
-## FIXME: add validity, check for GRanges, not GRangesList, meth, unmeth
+
+setValidity("GenomicMethylSet", function(object) {
+    msg <- validMsg(NULL, .checkAssayNames(object, c("Meth", "Unmeth")))
+    if(class(rowData(object)) != "GRanges")
+        msg <- validMsg(msg, sprintf("object of class '%s' needs to have a 'GRanges' in slot 'rowData'", class(object)))
+    if (is.null(msg)) TRUE else msg
+})
 
 GenomicMethylSet <- function(gr, Meth, Unmeth, pData, annotation, preprocessMethod) {
     assays <- SimpleList(Meth = Meth, Unmeth = Unmeth)
@@ -42,7 +48,7 @@ setMethod("getBeta", signature(object = "GenomicMethylSet"),
           })
 
 setMethod("getM", signature(object = "GenomicMethylSet"),
-          function (object, ...) {
+          function (object, type = "", ...) {
               if(type == "")
                   return(log2(getMeth(object) / getUnmeth(object)))
               if(type == "beta" || type == "Beta")
@@ -63,6 +69,16 @@ setMethod("pData", signature("GenomicMethylSet"),
           function(object) {
               colData(object)
           })
+
+setReplaceMethod("pData", c("GenomicMethylSet", "DataFrame"),
+    function(x, ..., value)
+{
+    x <- clone(x, ..., colData=value)
+    msg <- GenomicRanges:::.valid.SummarizedExperiment.colData_dims(x)
+    if (!is.null(msg))
+        stop(msg)
+    x
+})
 
 setMethod("sampleNames", signature("GenomicMethylSet"),
           function(object) {
