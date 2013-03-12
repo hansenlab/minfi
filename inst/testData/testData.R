89bb8713fb8377887e46c328b3087508289b8770
khansen <khansen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-03-12 14:29:01 +0000

    updating tests
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/minfi@74224 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/testData/testData.R
+++ inst/testData/testData.R
@@ -2,14 +2,14 @@ library(minfiData)
 library(digest)
 Mset.raw <- preprocessRaw(RGsetEx)
 Mset.swan <- preprocessSWAN(RGsetEx)
-Mset.illumina <- preprocessSWAN(RGsetEx)
+Mset.illumina <- preprocessIllumina(RGsetEx)
 
 testDigests <- list(
     preprocessRaw = list(Meth = digest(getMeth(Mset.raw)),
     Unmeth = digest(getUnmeth(Mset.raw))),
     preprocessIllumina = list(Meth = digest(getMeth(Mset.illumina)),
     Unmeth = digest(getUnmeth(Mset.illumina))),
-    preprocessSwan = list(Meth = digest(getMeth(Mset.swan)),
+    preprocessSWAN = list(Meth = digest(getMeth(Mset.swan)),
     Unmeth = digest(getUnmeth(Mset.swan)))
     )
 
