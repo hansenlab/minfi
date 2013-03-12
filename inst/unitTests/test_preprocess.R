f7fc78df55f678cadb5cc882257d878ec6e91270
Kasper D. Hansen <khansen@stat.Berkeley.edu>
2013-03-12 14:29:01 +0000

    updating tests
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/minfi@74224 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_preprocess.R
+++ inst/unitTests/test_preprocess.R
@@ -1,13 +1,35 @@
 test_preprocessRaw <- function() {
     stopifnot(require(minfiData))
     stopifnot(require(digest))
-    load("testDigests.rda")
+    load(file.path(path.package("minfi"), "unitTests", "testDigests.rda"))
     Mset <- preprocessRaw(RGsetEx)
     checkEquals(testDigests$preprocessRaw$Meth,
                 digest(getMeth(Mset)))
     checkEquals(testDigests$preprocessRaw$Unmeth,
                 digest(getUnmeth(Mset)))
 }
+
+test_preprocessIllumina <- function() {
+    stopifnot(require(minfiData))
+    stopifnot(require(digest))
+    load(file.path(path.package("minfi"), "unitTests", "testDigests.rda"))
+    Mset <- preprocessIllumina(RGsetEx)
+    checkEquals(testDigests$preprocessIllumina$Meth,
+                digest(getMeth(Mset)))
+    checkEquals(testDigests$preprocessIllumina$Unmeth,
+                digest(getUnmeth(Mset)))
+}
+
+## test_preprocessSWAN <- function() {
+##     stopifnot(require(minfiData))
+##     stopifnot(require(digest))
+##     load(file.path(path.package("minfi"), "unitTests", "testDigests.rda"))
+##     Mset <- preprocessSWAN(RGsetEx)
+##     checkEquals(testDigests$preprocessSwan$Meth,
+##                 digest(getMeth(Mset)))
+##     checkEquals(testDigests$preprocessSwan$Unmeth,
+##                 digest(getUnmeth(Mset)))
+## }
 
 
 
