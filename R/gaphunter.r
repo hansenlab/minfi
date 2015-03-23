fcbd74d8b0c0ea82abe1d6eac4387783683a7dd4
Shan Andrews <sandre17@jhu.edu>
2015-03-22 20:30:20 -0400

    Reworking of function for speed, and changing man, description and namespace accordingly

--- R/gaphunter.r
+++ R/gaphunter.r
@@ -7,63 +7,46 @@ if ((outcutoff <= 0)|(outcutoff >= 0.5))
 	stop("[gaphunter] 'outcutoff' must be between 0 and 0.5.") 
 if (is.null(Beta)){
 	if(verbose)
-		message("[gaphunter] Calculating beta matrix")
+		message("[gaphunter] Calculating beta matrix.")
 		Beta<-getBeta(object)
 	}
-if (verbose)
+if (verbose){
 	message("[gaphunter] Using ",prettyNum(nrow(Beta),big.mark=",",scientific=FALSE)," probes and ",prettyNum(ncol(Beta),big.mark=",",scientific=FALSE)," samples.")
-if (!getDoParRegistered())
-        registerDoSEQ()
-    workers <- getDoParWorkers()
-    backend <- getDoParName()
-    version <- getDoParVersion()
-    if (verbose) {
-        if (workers == 1) {
-            mes <- "[gaphunter] Using a single core (backend: %s, version: %s)."
-            message(sprintf(mes, backend, version))
-        }
-        else {
-            mes <- "[gaphunter] Parallelizing using %s workers/cores (backend: %s, version: %s)."
-            message(sprintf(mes, workers, backend, version))
-        }
-    }
-if (verbose)
 	message("[gaphunter] Searching for gap signals.")
-	
-chunksize <- ceiling(nrow(Beta)/workers) 
+}
 
-tmp <- foreach(probes = iter(Beta, by = "row", chunksize = chunksize)) %dorng%
-	{apply(probes,1,function(k){
-		atprobe<-sort(as.numeric(k))
-		diffprobe<-as.numeric(lapply(2:ncol(Beta),function(x){atprobe[x]-atprobe[(x-1)]}))
-		gaps<-(sum(diffprobe>threshold))
-		if (gaps>0){
-			grouptemp<-rep(1,ncol(Beta))
-			gaptemp<-rep(0,(gaps+2))
-			breakpoint<-which(diffprobe>threshold)
-			for (j in 1:length(breakpoint)){
-				sortpersonbreak<-breakpoint[j]+1
-				personbreak<-which(k==atprobe[sortpersonbreak])[1]
-				clust1<-which(k<k[personbreak])
-				clust2<-which(k==k[personbreak]|k>k[personbreak])
-				grouptemp[clust2]<-(j+1)
-			}
-			gaptemp[1]<-gaps+1
-			gaptemp[2:(2+gaps)]<-table(grouptemp)
-		return(list(gapanno = gaptemp,groupanno = grouptemp)) 
-		}
+sorting<-t(apply(Beta,1,sort.int,method="quick",index.return=TRUE))
+sortedindices<-lapply(sorting,function(n)return(n$ix))
+sortedbeta<-do.call("rbind",lapply(sorting,function(n)return(n$x)))
+rownames(sortedbeta)<-rownames(Beta)
+diffs<-rowDiffs(sortedbeta)
+gapind<-rowSums(diffs>threshold)
+sortedindices<-lapply(which(gapind>0),function(h){return(sortedindices[[h]])})
+sortedbeta<-sortedbeta[which(gapind>0),]
+diffs<-diffs[which(gapind>0),]
+gapind<-gapind[which(gapind>0)]
+
+breakpoints<-apply(diffs,1,function(x){return(which(x>threshold))})
+
+returngroups<-function(x,y){
+	template<-rep(1,ncol(sortedbeta))
+	count<-2
+	for (j in x){
+	template[y[(j+1):length(y)]]<-count
+	count<-count+1
 	}
-	)
-	}
-tmp<-unlist(tmp,recursive=FALSE)
-names(tmp)<-rownames(Beta)
-myout<-Filter(Negate(is.null),tmp)
-groupanno<-do.call("rbind",lapply(myout,function(x)return(x$groupanno)))
-pregaps<-lapply(myout,function(x)return(x$gapanno))
-mymax<-max(unlist(lapply(pregaps,length)))
-gapanno<-do.call("rbind",lapply(pregaps,function(x){length(x)<-mymax; return(x)}))
-rownames(gapanno)<-names(myout)
-rownames(groupanno)<-names(myout)
+	return(template)
+}	
+
+groupanno<-t(mapply(returngroups,breakpoints,sortedindices))
+gapanno<-apply(groupanno,1,function(k){
+	tempgap<-rep(0,(length(unique(k))+1))
+	tempgap[1]<-length(unique(k))
+	tempgap[2:length(tempgap)]<-table(k)
+	return(tempgap)})
+gapanno<-do.call("rbind",lapply(gapanno,function(x){length(x)<-(max(gapind)+2); return(x)})) 
+rownames(gapanno)<-rownames(sortedbeta) ##
+rownames(groupanno)<-rownames(sortedbeta)
 
 if(verbose)
 message("[gaphunter] Found ",prettyNum(nrow(gapanno),big.mark=",",scientific=FALSE)," gap signals.")
@@ -76,15 +59,15 @@ if (keepoutliers==FALSE){
 		analyze<-gapanno[blah,(2:(1+numgroup))]
 		maxgroup<-which(analyze==max(analyze))
 		if (length(maxgroup)==1){	
-			if(sum(analyze[-maxgroup])<outcutoff*ncol(Beta)){
-			return(blah)}
+			if(sum(analyze[-maxgroup])<outcutoff*ncol(Beta))			
+			{return(blah)}
 		}}))
 	if (length(markme)>0){
 	gapanno<-gapanno[-markme,]
 	groupanno<-groupanno[-markme,]}
 	if (verbose)
-	message("[gaphunter] Removed ",prettyNum(length(markme),big.mark=",",scientific=FALSE)," gaps driven by outliers from results.")
-}
+	message("[gaphunter] Removed ",prettyNum(length(markme),big.mark=",",scientific=FALSE)," gap signals driven by outliers from results.")
+}	
 gapanno<-data.frame(gapanno)
 
 labs<-c()
