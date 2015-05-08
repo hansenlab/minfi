087f803466aca055b48e5822daad37c4fb50b536
Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
2015-05-08 13:16:07 -0400

    using new split digest files

--- inst/unitTests/test_preprocess.R
+++ inst/unitTests/test_preprocess.R
@@ -1,47 +1,44 @@
 test_preprocessRaw <- function() {
     stopifnot(require(minfiData))
     stopifnot(require(digest))
-    load(file.path(path.package("minfi"), "unitTests", "testDigests.rda"))
+    load(file.path(path.package("minfi"), "unitTests", "digest_preprocessRaw.rda"))
     Mset <- preprocessRaw(RGsetEx)
-    checkEquals(testDigests$preprocessRaw$Meth,
+    checkEquals(digest_preprocessRaw$Meth,
                 minfi:::.digestMatrix(getMeth(Mset)))
-    checkEquals(testDigests$preprocessRaw$Unmeth,
+    checkEquals(digest_preprocessRaw$Unmeth,
                 minfi:::.digestMatrix(getUnmeth(Mset)))
 }
 
 test_preprocessIllumina <- function() {
     stopifnot(require(minfiData))
     stopifnot(require(digest))
-    load(file.path(path.package("minfi"), "unitTests", "testDigests.rda"))
+    load(file.path(path.package("minfi"), "unitTests", "digest_preprocessIllumina.rda"))
     Mset <- preprocessIllumina(RGsetEx)
-    checkEquals(testDigests$preprocessIllumina$Meth,
+    checkEquals(digest_preprocessIllumina$Meth,
                 minfi:::.digestMatrix(getMeth(Mset)))
-    checkEquals(testDigests$preprocessIllumina$Unmeth,
+    checkEquals(digest_preprocessIllumina$Unmeth,
                 minfi:::.digestMatrix(getUnmeth(Mset)))
 }
 
 test_preprocessSWAN <- function() {
     stopifnot(require(minfiData))
     stopifnot(require(digest))
-    load(file.path(path.package("minfi"), "unitTests", "testDigests.rda"))
+    load(file.path(path.package("minfi"), "unitTests", "digest_preprocessSWAN.rda"))
     set.seed(456)
     Mset <- preprocessSWAN(RGsetEx)
-    checkEquals(testDigests$preprocessSWAN$Meth,
+    checkEquals(digest_preprocessSWAN$Meth,
                 minfi:::.digestMatrix(getMeth(Mset)))
-    checkEquals(testDigests$preprocessSWAN$Unmeth,
+    checkEquals(digest_preprocessSWAN$Unmeth,
                 minfi:::.digestMatrix(getUnmeth(Mset)))
 }
 
 test_preprocessQuantile <- function() {
     stopifnot(require(minfiData))
     stopifnot(require(digest))
-    load(file.path(path.package("minfi"), "unitTests", "testDigests.rda"))
+    load(file.path(path.package("minfi"), "unitTests", "digest_preprocessQuantile.rda"))
     GMset <- preprocessQuantile(MsetEx)
-    checkEquals(testDigests$preprocessQuantile$M,
+    checkEquals(digest_preprocessQuantile$M,
                 minfi:::.digestMatrix(getM(GMset)))
-    checkEquals(testDigests$preprocessQuantile$CN,
+    checkEquals(digest_preprocessQuantile$CN,
                 minfi:::.digestMatrix(getCN(GMset)))
 }
-
-
-
