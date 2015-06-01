0caba8cbdd469d4f39c712c9b038f10d6120fe27
Kasper D. Hansen <khansen@stat.Berkeley.edu>
2015-06-01 03:12:53 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 950700df6804d3fc3b007dd20df7d5fdd41789d1
    
        trying to fix compartments test
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/minfi@104410 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/testData/testData_compartments.R
+++ inst/testData/testData_compartments.R
@@ -5,7 +5,7 @@ gr.cor <- minfi:::createCorMatrix(MsetEx, res=500*1000)
 gr.ab <- minfi:::extractAB(gr.cor)
 
 digest_compartments <- list(cor.matrix = minfi:::.digestMatrix(gr.cor$cor.matrix),
-                           pc = minfi:::.digestVector(gr.ab$pc))
+                           pc = minfi:::.digestVector(gr.ab$pc, digits = 3))
 save(digest_compartments, file = "../unitTests/digest_compartments.rda")
 
 sessionInfo()                         
