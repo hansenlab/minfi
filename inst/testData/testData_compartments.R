90df8edc8816b3ce9b0b50c80f441e872201cc37
Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
2015-07-14 14:54:41 -0400

    fixes to compartment unit testing

--- inst/testData/testData_compartments.R
+++ inst/testData/testData_compartments.R
@@ -1,8 +1,9 @@
 library(minfiData)
 library(digest)
 
-gr.cor <- minfi:::createCorMatrix(MsetEx, res=500*1000)
-gr.ab <- minfi:::extractAB(gr.cor)
+GMsetEx <- mapToGenome(MsetEx)
+gr.cor <- createCorMatrix(GMsetEx, res=500*1000)
+gr.ab <- extractAB(gr.cor)
 
 digest_compartments <- list(cor.matrix = minfi:::.digestMatrix(gr.cor$cor.matrix),
                            pc = minfi:::.digestVector(gr.ab$pc))
--- inst/unitTests/test_compartments.R
+++ inst/unitTests/test_compartments.R
@@ -3,10 +3,10 @@ test_compartments <- function() {
     stopifnot(require(digest))
     load(file.path(path.package("minfi"), "unitTests", "digest_compartments.rda"))
     GMsetEx <- mapToGenome(MsetEx)
-    gr.cor <- minfi:::createCorMatrix(GMsetEx, res=500*1000)
+    gr.cor <- createCorMatrix(GMsetEx, res=500*1000)
     checkEquals(digest_compartments$cor.matrix,
                 minfi:::.digestMatrix(gr.cor$cor.matrix))
-    gr.ab <- minfi:::extractAB(gr.cor)
+    gr.ab <- extractAB(gr.cor)
     checkEquals(digest_compartments$pc,
                 minfi:::.digestVector(gr.ab$pc))
 }
