6b859f9257509a49b7b899bb85c49068ac96bffe
Kasper D. Hansen <khansen@stat.Berkeley.edu>
2016-02-19 17:15:10 +0000

    starting to support EPIC arrays
    From: Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/minfi@113812 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/read.manifest.R
+++ R/read.manifest.R
@@ -1,16 +1,23 @@
 read.manifest <- function(file) {
     ## As is, requires grep
-    control.line <- system(sprintf("grep -n Controls %s", file), intern = TRUE)
+    control.line <- system(sprintf("grep -n \\\\[Controls\\\\] %s", file), intern = TRUE)
     control.line <- as.integer(sub(":.*", "", control.line))
+    stopifnot(length(control.line) == 1 && is.integer(control.line) && !is.na(control.line))
+    assay.line <- system(sprintf("grep -n \\\\[Assay\\\\] %s", file), intern = TRUE)
+    assay.line <- as.integer(sub(":.*", "", assay.line))
+    stopifnot(length(assay.line) == 1 && is.integer(assay.line) && !is.na(assay.line))
+   
     ## Column headers is in line 8, hardcoded
-    colNames <- readLines(file, n = 8)[8]
+    colNames <- readLines(file, n = assay.line+1L)[assay.line+1L]
     colNames <- strsplit(colNames, ",")[[1]]
     colClasses <- rep("character", length(colNames))
-    names(colClasses) = colNames
+    names(colClasses) <- colNames
     colClasses[c("MAPINFO")] <- "integer"
-    manifest <- read.table(file, header = TRUE,
+    manifest <- read.table(file, header = FALSE,
                            sep = ",", comment.char = "", quote = "", 
-                           skip = 7, colClasses = colClasses, nrows = control.line - 9)
+                           skip = assay.line + 1L, colClasses = colClasses,
+                           nrows = control.line - assay.line - 2L)
+    names(manifest) <- names(colClasses)
     TypeI <- manifest[manifest$Infinium_Design_Type == "I",
                         c("Name", "AddressA_ID", "AddressB_ID", "Color_Channel",
                           "Next_Base", "AlleleA_ProbeSeq", "AlleleB_ProbeSeq")]
@@ -38,11 +45,11 @@ read.manifest <- function(file) {
     
     controls <- read.table(file, skip = control.line,
                            sep = ",", comment.char = "", quote = "",
-                           colClasses = c(rep("character", 5)))
+                           colClasses = c(rep("character", 5)))[, 1:5]
     TypeControl <- controls[, 1:4]
     names(TypeControl) <- c("Address", "Type", "Color", "ExtendedType")
     TypeControl <- as(TypeControl, "DataFrame")
-    
+
     list(manifestList = list(TypeI = TypeI, TypeII = TypeII, TypeControl = TypeControl,
          TypeSnpI = TypeSnpI, TypeSnpII = TypeSnpII),
          manifest = manifest, controls = controls)
