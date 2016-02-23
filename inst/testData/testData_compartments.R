666a4942cd6924e4e79cfa03f85fb73ed1cad79d
Kasper D. Hansen <khansen@stat.Berkeley.edu>
2016-02-23 20:13:42 +0000

    merged from master
    From: Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/minfi@113941 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/testData/testData_compartments.R
+++ inst/testData/testData_compartments.R
@@ -3,10 +3,11 @@ library(digest)
 
 GMsetEx <- mapToGenome(MsetEx)
 gr.cor <- createCorMatrix(GMsetEx, res=500*1000)
+set.seed(456)
 gr.ab <- extractAB(gr.cor)
 
 digest_compartments <- list(cor.matrix = minfi:::.digestMatrix(gr.cor$cor.matrix),
-                           pc = minfi:::.digestVector(gr.ab$pc))
+                           pc = minfi:::.digestVector(gr.ab$pc, digits = 2))
 save(digest_compartments, file = "../unitTests/digest_compartments.rda")
 
 sessionInfo()                         
--- inst/unitTests/test_compartments.R
+++ inst/unitTests/test_compartments.R
@@ -6,8 +6,9 @@ test_compartments <- function() {
     gr.cor <- createCorMatrix(GMsetEx, res=500*1000)
     checkEquals(digest_compartments$cor.matrix,
                 minfi:::.digestMatrix(gr.cor$cor.matrix))
+    set.seed(456)
     gr.ab <- extractAB(gr.cor)
     checkEquals(digest_compartments$pc,
-                minfi:::.digestVector(gr.ab$pc))
+                minfi:::.digestVector(gr.ab$pc, digits = 2))
 }
 
