c5ff32a6aeabfef1ad9c54a139b08cdeaec440fb
Peter Hickey <peter.hickey@gmail.com>
2017-05-20 16:39:07 -0400

    Initial pass over dmr.R to support DelayedArray

--- R/dmr.R
+++ R/dmr.R
@@ -1,42 +1,50 @@
+# TODO: Could do this in rowwise chunks to avoid realizing all of M in memory
 dmpFinder <- function(dat, pheno, type = c("categorical", "continuous"),
                       qCutoff = 1, shrinkVar = FALSE) {
     type <- match.arg(type)
     if (is(dat, "MethylSet")) {
         M <- getM(dat)
     } else {
-        stopifnot(is.numeric(dat))
+        # TODO: namespace clash between DelayedArray::type() and
+        #       Biostrings::type()?
+        stopifnot(DelayedArray::type(dat) %in% c("integer", "double"))
         M <- dat
-        if (is.vector(M)) M <- matrix(M, nrow=1)
     }
     n <- length(pheno)
-    if (n != ncol(M)) 
+    if (n != ncol(M))
         stop("length of pheno does not equal number of samples")
     if (type == "categorical") {
         pheno <- factor(as.character(pheno))
         design <- model.matrix(~pheno)
+        # NOTE: lmFit() realizes M in memory as an array
         fit <- lmFit(M, design)
         if (shrinkVar) {
             fit <- contrasts.fit(fit, contrasts(pheno))
             fit <- eBayes(fit)
-            tab <- data.frame(intercept=fit$coefficients[, 1], f=fit[["F"]], pval=fit[["F.p.value"]])
+            tab <- data.frame(intercept = fit$coefficients[, 1],
+                              f = fit[["F"]],
+                              pval = fit[["F.p.value"]])
         } else {
+            # NOTE: lmFit() realizes M in memory as an array
             fit1 <- lmFit(M)
-            RSS1 <- rowSums((M - fitted(fit1))^2)
-            RSS <- rowSums((M - fitted(fit))^2)
+            RSS1 <- rowSums((M - DelayedArray(fitted(fit1))) ^ 2)
+            RSS <- rowSums((M - DelayedArray(fitted(fit))) ^ 2)
             df1 <- length(levels(pheno)) - 1
             df2 <- n - length(levels(pheno))
             Fstat <- ((RSS1 - RSS)/df1)/(RSS/df2)
             if (df2 > 1e+06) {
-                F.p.value <- pchisq(df1 * Fstat, df1, lower.tail=FALSE)
+                F.p.value <- pchisq(df1 * Fstat, df1, lower.tail = FALSE)
+            } else {
+                F.p.value <- pf(Fstat, df1, df2, lower.tail = FALSE)
             }
-            else {
-                F.p.value <- pf(Fstat, df1, df2, lower.tail=FALSE)
-            }
-            tab <- data.frame(intercept=fit$coefficients[, 1], f=Fstat, pval=F.p.value)
+            tab <- data.frame(intercept = fit$coefficients[, 1],
+                              f = Fstat,
+                              pval = F.p.value)
         }
     }
     else if (type == "continuous") {
         design <- model.matrix(~pheno)
+        # NOTE: lmFit() realizes M in memory as an array
         fit <- lmFit(M, design)
         if (shrinkVar) {
             fit <- eBayes(fit)
@@ -47,10 +55,10 @@ dmpFinder <- function(dat, pheno, type = c("categorical", "continuous"),
             df <- fit$df.residual
         }
         coef <- fit$coefficients
-        if (is.vector(coef)) 
+        if (is.vector(coef))
             coef <- matrix(coef, ncol=2)
         stdev <- fit$stdev.unscaled
-        if (is.vector(stdev)) 
+        if (is.vector(stdev))
             stdev <- matrix(stdev, ncol=2)
         t <- coef[, 2]/(stdev[, 2] * sigma)
         pval <- 2 * pt(abs(t), df=df, lower.tail=FALSE)
@@ -58,12 +66,12 @@ dmpFinder <- function(dat, pheno, type = c("categorical", "continuous"),
     }
     p0 <- pi0.est(tab$pval[!is.na(tab$pval)])$p0
     tab$qval <- qvalue.cal(tab$pval, p0)
-    if (qCutoff < 1) 
+    if (qCutoff < 1)
         tab <- tab[tab$qval <= qCutoff,]
     o <- order(tab$pval)
     tab <- tab[o, ]
     ## tab <- cbind(tab, annotate(rownames(tab)))
-    if (nrow(tab) == 0) 
+    if (nrow(tab) == 0)
         message(sprintf("No significant DMPs at FDR cutoff of %s.", qCutoff))
     tab
 }
