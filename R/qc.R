2896a0549aca5676c98119af29b0353bc2e1b8cb
Peter Hickey <peter.hickey@gmail.com>
2018-03-28 14:40:22 -0400

    Update qc.R to support DelayedArray-backed minfi objects

--- R/qc.R
+++ R/qc.R
@@ -1,3 +1,6 @@
+# TODO: Calling `qcReport(rgSet)` results in a PDF where no data are plotted for the first 2
+#       pages. I think it's because sampGroups is NULL by default. This is broken in the
+#       version of minfi available from Bioconductor, not just in this branch
 qcReport <- function(rgSet, sampNames=NULL, sampGroups=NULL, pdf="qcReport.pdf", maxSamplesPerPage=24,
         controls=c("BISULFITE CONVERSION I", "BISULFITE CONVERSION II", "EXTENSION", "HYBRIDIZATION",
         "NON-POLYMORPHIC", "SPECIFICITY I", "SPECIFICITY II", "TARGET REMOVAL")) {
@@ -65,14 +68,16 @@ controlStripPlot <- function(rgSet, controls=c("BISULFITE CONVERSION I", "BISULF
     }
 }
 
-
 densityBeanPlot <- function(dat, sampGroups=NULL, sampNames=NULL, main=NULL, pal=brewer.pal(8, "Dark2"), numPositions=10000) {
     if (is(dat, "RGChannelSet") || is(dat, "MethylSet")) {
         b <- getBeta(dat)
     } else if (is(dat, "matrix")) {
         b <- dat
+    } else if (is(dat, "DelayedMatrix")) {
+        b <- dat
     } else {
-        stop("argument 'dat' must be an 'RGChannelSet', a 'MethylSet'  or matrix.")
+        stop("argument 'dat' must be an 'RGChannelSet', a 'MethylSet', ",
+             "a 'DelayedMatrix', or 'matrix'.")
     }
     n <- ncol(b)
     if (!is.null(sampNames)) colnames(b) <- sampNames
@@ -80,11 +85,16 @@ densityBeanPlot <- function(dat, sampGroups=NULL, sampNames=NULL, main=NULL, pal
     if (is.null(sampGroups))  sampGroups <- rep(1, n)
     sampGroups <- as.factor(sampGroups)
     col <- lapply(sampGroups, function(x) rep(pal[x],4))
-    if(is.null(numPositions))
+    if (is.null(numPositions)) {
         idx <- 1:dim(dat)[1]
-    else
+    } else {
         idx <- sample(nrow(b), numPositions)
-    x <- melt(b[idx, ], varnames=c("cpg", "sample"))
+    }
+    # NOTE: At this point, the data need to be in-memory as an ordinary matrix
+    # TODO: Update in response to any suggestions in
+    #       https://github.com/Bioconductor/DelayedArray/issues/13
+    b_subset <- as.matrix(b[idx, ])
+    x <- melt(b_subset, varnames=c("cpg", "sample"))
     o <- order(colnames(b))
     beanplot(value ~ sample, horizontal=TRUE, what=c(0,1,1,0), log="", las=1, ylim=c(0,1),
         xlab="Beta", main=main, col=col[o], data=x, cex.lab=0.9, beanlinewd=1, border=NA)
@@ -100,7 +110,9 @@ densityPlot <- function(dat, sampGroups=NULL, main="", xlab="Beta", pal=brewer.p
     } else {
         stop("argument 'dat' must be an 'RGChannelSet', a 'MethylSet'  or matrix.")
     }
-    d <- apply(b, 2, density, na.rm=TRUE)
+    # NOTE: Have to ensure data are in memory as an ordinary vector before computing density
+    # TODO: Could speed up using bplapply()
+    d <- apply(b, 2, function(x) density(as.vector(x), na.rm = TRUE))
     if (missing(ylim)) ylim <- range(sapply(d, function(i) range(i$y)))
     if (missing(xlim)) xlim <- range(sapply(d, function(i) range(i$x)))
     if (is.null(sampGroups)) {
