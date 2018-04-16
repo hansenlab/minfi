90cfc9f941a47ea3b1f4f00db190f429006553dc
Peter Hickey <peter.hickey@gmail.com>
2018-04-16 11:08:26 -0400

    Rename dmr.R dmpFinder.R and add check for DelayedArray-backed input

--- R/dmpFinder.R
+++ R/dmpFinder.R
@@ -1,7 +1,12 @@
+# Exported functions -----------------------------------------------------------
+
 dmpFinder <- function(dat, pheno, type = c("categorical", "continuous"),
                       qCutoff = 1, shrinkVar = FALSE) {
+
+    # Check inputs
     type <- match.arg(type)
     if (is(dat, "MethylSet")) {
+        .supportsDelayedArray(dat)
         M <- getM(dat)
     } else {
         stopifnot(is.numeric(dat))
@@ -9,8 +14,8 @@ dmpFinder <- function(dat, pheno, type = c("categorical", "continuous"),
         if (is.vector(M)) M <- matrix(M, nrow=1)
     }
     n <- length(pheno)
-    if (n != ncol(M)) 
-        stop("length of pheno does not equal number of samples")
+    if (n != ncol(M)) stop("length of pheno does not equal number of samples")
+
     if (type == "categorical") {
         pheno <- factor(as.character(pheno))
         design <- model.matrix(~pheno)
@@ -18,7 +23,10 @@ dmpFinder <- function(dat, pheno, type = c("categorical", "continuous"),
         if (shrinkVar) {
             fit <- contrasts.fit(fit, contrasts(pheno))
             fit <- eBayes(fit)
-            tab <- data.frame(intercept=fit$coefficients[, 1], f=fit[["F"]], pval=fit[["F.p.value"]])
+            tab <- data.frame(
+                intercept=fit$coefficients[, 1],
+                f = fit[["F"]],
+                pval = fit[["F.p.value"]])
         } else {
             fit1 <- lmFit(M)
             RSS1 <- rowSums((M - fitted(fit1))^2)
@@ -32,7 +40,10 @@ dmpFinder <- function(dat, pheno, type = c("categorical", "continuous"),
             else {
                 F.p.value <- pf(Fstat, df1, df2, lower.tail=FALSE)
             }
-            tab <- data.frame(intercept=fit$coefficients[, 1], f=Fstat, pval=F.p.value)
+            tab <- data.frame(
+                intercept = fit$coefficients[, 1],
+                f = Fstat,
+                pval = F.p.value)
         }
     }
     else if (type == "continuous") {
@@ -47,24 +58,25 @@ dmpFinder <- function(dat, pheno, type = c("categorical", "continuous"),
             df <- fit$df.residual
         }
         coef <- fit$coefficients
-        if (is.vector(coef)) 
-            coef <- matrix(coef, ncol=2)
+        if (is.vector(coef)) coef <- matrix(coef, ncol = 2)
         stdev <- fit$stdev.unscaled
-        if (is.vector(stdev)) 
-            stdev <- matrix(stdev, ncol=2)
-        t <- coef[, 2]/(stdev[, 2] * sigma)
-        pval <- 2 * pt(abs(t), df=df, lower.tail=FALSE)
-        tab <- data.frame(intercept=coef[, 1], beta=coef[, 2], t=t, pval=pval)
+        if (is.vector(stdev)) stdev <- matrix(stdev, ncol = 2)
+        t <- coef[, 2] / (stdev[, 2] * sigma)
+        pval <- 2 * pt(abs(t), df=df, lower.tail = FALSE)
+        tab <- data.frame(
+            intercept = coef[, 1],
+            beta = coef[, 2],
+            t = t,
+            pval = pval)
     }
     p0 <- pi0.est(tab$pval[!is.na(tab$pval)])$p0
     tab$qval <- qvalue.cal(tab$pval, p0)
-    if (qCutoff < 1) 
-        tab <- tab[tab$qval <= qCutoff,]
+    if (qCutoff < 1) tab <- tab[tab$qval <= qCutoff,]
     o <- order(tab$pval)
     tab <- tab[o, ]
     ## tab <- cbind(tab, annotate(rownames(tab)))
-    if (nrow(tab) == 0) 
+    if (nrow(tab) == 0) {
         message(sprintf("No significant DMPs at FDR cutoff of %s.", qCutoff))
+    }
     tab
 }
-
